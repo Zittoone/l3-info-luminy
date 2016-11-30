ae50f1a7115cf6f988687aff269ae94e1dc2d8fa
Sébastien Tosello <sebtosello@gmail.com>
2016-11-30 17:16:35 +0100

    Devoir3: Modifs Edge class

--- AlgoAv/Devoir3/src/main/java/fr/licinfo/Edge.java
+++ AlgoAv/Devoir3/src/main/java/fr/licinfo/Edge.java
@@ -4,31 +4,83 @@ package fr.licinfo;
  * Created by sebastientosello on 23/11/2016.
  */
 public class Edge {
-    private int connectedVertice, weight;
+    private int cap, flow, dest, source ;
 
-    public Edge(int connectedVertice, int weight){
-        this.weight = weight;
-        this.connectedVertice = connectedVertice;
+    public Edge(int source, int dest, int cap, int flow){
+        this.source = source;
+        this.dest = dest;
+        this.cap = cap;
+        this.flow = flow;
     }
 
-    public int getConnectedVertice(){
-        return this.connectedVertice;
+    public Edge(int source, int dest, int flow){
+        this.source = source;
+        this.dest = dest;
+        this.flow = flow;
     }
 
-    public void setConnectedVertice(int connectedVertice) {
-        this.connectedVertice = connectedVertice;
+    public Edge(Edge e){
+        this.source = e.getSource();
+        this.dest = e.getDest();
+        this.cap = e.getCap();
+        this.flow = e.getFlow();
     }
 
-    public int getWeight() {
-        return weight;
+    public int getCap() {
+        return cap;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
+    public int getFlow() {
+        return flow;
     }
 
+    public int getDest() {
+        return dest;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public void setFlow(int flow) {
+        this.flow = flow;
+    }
+
+    @Override
     public String toString(){
-        return weight + "->" + connectedVertice;
+        return dest + "--" + flow + "/" + cap + "-->" + dest;
     }
 
+    @Override
+    public boolean equals(Object o){
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Edge)){
+            return false;
+        }
+
+        if(((Edge) o).getCap() != this.getCap()){
+            return false;
+        }
+
+        if(((Edge) o).getDest() != this.getDest()){
+            return false;
+        }
+
+        if(((Edge) o).getFlow() != this.getFlow()){
+            return false;
+        }
+
+        if(((Edge) o).getSource() != this.getSource()){
+            return false;
+        }
+
+        return true;
+    }
 }
