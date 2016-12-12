ec237a4787f21d1da19e297f15181978a52ee51a
Alexis COUVREUR <c16017548@tempo21-2.luminy.univ-amu.fr>
2016-12-12 17:08:05 +0100

    TP1.2 CBDD suite

--- CBDD/TP1/querries_partie_2.sql
+++ CBDD/TP1/querries_partie_2.sql
@@ -25,9 +25,9 @@ BEGIN
   RETURN NumEnsFerm;
   END;
 
-  ----II
-  /* EstDeductible */
-  create or replace FUNCTION EstDeductible(p_NumDF NUMBER, p_NumEnsDF NUMBER)
+----II
+/* EstDeductible */
+create or replace FUNCTION EstDeductible(p_NumDF NUMBER, p_NumEnsDF NUMBER)
 RETURN INTEGER IS
 NumEnsG NUMBER;
 NumEnsD NUMBER;
@@ -40,3 +40,65 @@ BEGIN
     END IF;
     RETURN 0;
   END;
+
+----III
+/* UneClef */
+CREATE FUNCTION UneClef(p_NumEnsAtt NUMBER, p_NumEnsDF NUMBER)
+RETURN NUMBER IS
+NumEnsAtt NUMBER;
+NumEnsAttSoustr NUMBER;
+BEGIN
+  NumEnsAtt := CopieAtt(p_NumEnsAtt);
+  FOR i IN (SELECT * FROM EnsembleContientAttribut
+            WHERE NumEnsAtt = p_NumEnsAtt)
+  LOOP
+    COMMIT;
+    NumEnsAttSoustr := SoustractionAtt(NumEnsAtt, i.NumEnsAtt);
+    IF EstInclus(NumEnsAttSoustr, FermetureAtt(p_NumEnsAtt, p_NumEnsDF)) = 1
+    THEN NumEnsAtt := NumEnsAttSoustr;
+    ELSE ROLLBACK;
+    END IF;
+    END LOOP;        
+END;
+
+
+----IV
+/* ToutesLesClefs */
+CREATE PROCEDURE ToutesLesClefs(p_NumSchema NUMBER) IS
+NumEnsClef NUMBER;
+BEGIN
+  NumEnsClef := CreerEnsClefVide(p_NumSchema);
+  FOR i IN (SELECT * FROM SCHEMAS
+            WHERE NumSchema = p_NumSchema)
+  LOOP
+  AjouterClef(UneClef(i.NumEnsAtt, i.NumEnsDF), NumEnsClef);
+  END LOOP;
+END;
+
+----V
+/* FormeCanonique */
+create or replace FUNCTION FormeCanonique(p_NumEnsDF NUMBER)
+RETURN NUMBER IS
+NumEnsDF NUMBER;
+BoucleDF NUMBER;
+BoucleAtt NUMBER;
+NumEnsDroit NUMBER;
+BEGIN
+NumEnsDF := CopieEnsDF(p_NumEnsDF);
+FOR BoucleDF IN(SELECT * FROM EnsembleContientDF e
+         INNER JOIN DFS d ON e.NumDFS = d.NUMDFS
+         WHERE NumEnsDF = p_NumEnsDF)
+  LOOP
+/* NumEnsDF := Supprimer BoucleDF non implémenté */
+  FOR BoucleAtt IN(SELECT * FROM EnsembleContientAttribut
+                   WHERE NumEnsAtt = BoucleDF.NumEnsDroit)
+    LOOP
+      NumEnsDroit := CreerDF(BoucleAtt.NomAtt);
+      AjouterDF(NumEnsDroit, NumEnsDF);
+    END LOOP;
+  END LOOP;
+END;
+
+----VI
+/* FormeCompacte */
+
