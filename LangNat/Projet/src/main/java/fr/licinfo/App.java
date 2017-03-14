e49993ea33b2db9dad4e979ac77fc20368a3dd2b
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2017-03-14 23:28:16 +0100

    LangNat projet TreeTagger + Standford

--- LangNat/Projet/src/main/java/fr/licinfo/App.java
+++ LangNat/Projet/src/main/java/fr/licinfo/App.java
@@ -1,13 +1,29 @@
 package fr.licinfo;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.annolab.tt4j.TokenHandler;
+import org.annolab.tt4j.TreeTaggerWrapper;
+
+import static java.util.Arrays.asList;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+
+
+
+        System.setProperty("treetagger.home", "c:\\TreeTagger");
+        TreeTaggerWrapper tt = new TreeTaggerWrapper<String>();
+        try {
+            tt.setModel("c:\\TreeTagger\\lib\\french-utf8.par:iso8859-1");
+            tt.setHandler(new TokenHandler<String>() {
+                public void token(String token, String pos, String lemma) {
+                    System.out.println(token + "\t" + pos + "\t" + lemma);
+                }
+            });
+            tt.process(asList(new String[] { "Il", "est", "ton", "père", "." }));
+        }
+        finally {
+            tt.destroy();
+        }
     }
 }
